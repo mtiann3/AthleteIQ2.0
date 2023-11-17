8f22073fed066122091e4e744755867533486f9c
mtiann3 <120132854+mtiann3@users.noreply.github.com>
2023-11-17 14:29:16 -0600

    Update AthleticBld.jsx
    created algorithm to update progress for an exercise

--- src/pages/AthleticBld.jsx
+++ src/pages/AthleticBld.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import LineGraph from "../components/LineGraph";
+// algorithm to update progress across an exercise*********************************
+// 1. Filter all entered exercises by exercises entered in a 
+// specific month (start with january of the first year)
+// 2. Filter all of the filtered exercises by a specific exercise name.
+// 3. Go through each exercise, and convert them to an estimated 1 rep max, then add 
+// them to a temp array.
+// 4. Take the average of the temp array.
+// 5. Add that value to a new temp array.
+// 6. Add the month and year of that value to a separate array, but same index.
+// 6. Repeat for every month until every month with an entry is covered.
+// 7. Update data and options with the 2 temp arrays.
 
 const AthleticBld = () => {
   const seriesData = [
     {
       name: 'Pounds',
-      data: [10, 15, 20, 50, 70, 65, 100, 148],
+      data: [90,145,165,180,185,200,205,215,225,230,235,245,260,275],
     },
     // Add more series data if needed
   ];
@@ -36,7 +47,7 @@ const AthleticBld = () => {
       },
     },
     xaxis: {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      categories: ['9/2021', '10/2022', '11/2022', '12/2022', '1/2023', '2/2023', '3/2023', '4/2023', '5/2023','6/23','11/2022','11/2022'],
     },
   };
 
